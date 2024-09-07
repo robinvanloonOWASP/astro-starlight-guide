3c828ce81ee56af752187212e1063bd4f619938c
robinvanloonOWASP <robin.van.loon@owasp.org>
2024-09-07 20:12:15 +0200

    Publish got Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://robinvanloonowasp.github.io',
+	base: 'astro-starlight-guide',
 	integrations: [
 		starlight({
 			title: 'OWASP Security Champions Guide',
